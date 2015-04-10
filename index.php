1cf55b2ec2f34fcb753610e590682fa7981a2d75
Scott de Jonge <dejonge.scott@gmail.com>
2015-04-10 14:56:06 +1000

    Styles and Character Lsit

--- index.php
+++ index.php
@@ -7,6 +7,258 @@
 			'keys' => 'option + ]',
 			'entity' => '&lsquo;',
 		],
+		[
+			'character' => '’',
+			'title' => 'Closing Single Quote',
+			'keys' => 'option + shift + ]',
+			'entity' => '&rsquo;',
+		],
+		[
+			'character' => '“',
+			'title' => 'Opening Double Quote',
+			'keys' => 'option + [',
+			'entity' => '&ldquo;',
+		],
+		[
+			'character' => '”',
+			'title' => 'Opening Double Quote',
+			'keys' => 'option + shift + [',
+			'entity' => '&rdquo;',
+		],
+		[
+			'character' => '–',
+			'title' => 'En Dash',
+			'keys' => 'option + -',
+			'entity' => '&ndash;',
+		],
+		[
+			'character' => '—',
+			'title' => 'Em Dash',
+			'keys' => 'option + shift + -',
+			'entity' => '&mdash;',
+		],
+		[
+			'character' => '‒',
+			'title' => 'Figure Dash',
+			'keys' => '',
+			'entity' => '&#8210;',
+		],
+		[
+			'character' => '-',
+			'title' => 'Hyphen-Minus',
+			'keys' => '-',
+			'entity' => '',
+		],
+		[
+			'character' => '‐',
+			'title' => 'Hyphen',
+			'keys' => '',
+			'entity' => '&#8208;',
+		],
+		[
+			'character' => '−',
+			'title' => 'Minus Sign',
+			'keys' => '',
+			'entity' => '&minus;',
+		],
+		[
+			'character' => '†',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '‡',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '§',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '¶',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '❧',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '№',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '☞',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '…',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '•',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '·',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '←',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '↑',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '→',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '↓',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '↔',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '©',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '®',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '™',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '¢',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '£',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '€',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '°',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '½',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '±',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '×',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '÷',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '≈',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '≠',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '«',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '»',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '‹',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '›',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
+		[
+			'character' => '',
+			'title' => '',
+			'keys' => '',
+			'entity' => '',
+		],
 	];
 
 ?><!doctype html>
@@ -36,11 +288,16 @@
 				<tbody>
 
 				<?php foreach ($characters as $character) : ?>
+					<?php
+						$keys = explode(' + ', $character['keys']);
+						$keys = '<kbd>' . implode('</kbd> + <kbd>', $keys) . '</kbd>';
+						$code = '<code>' . htmlentities($character['entity']) . '</code>';
+					?>
 					<tr>
 						<td><?php echo $character['character']; ?></td>
 						<td><?php echo $character['title']; ?></td>
-						<td><kbd><?php echo $character['keys']; ?></kbd></td>
-						<td><code><?php echo htmlentities($character['entity']); ?></code></td>
+						<td><?php if (!empty($character['keys'])) echo $keys; ?></td>
+						<td><?php if (!empty($character['entity'])) echo $code; ?></code></td>
 					</tr>
 				<?php endforeach; ?>
 
